65989a44e93ee39d7fa08132019e11014837e81b
kimseeun10 <tpdms981@gmail.com>
2023-10-24 16:12:52 +0900

    수정 properties에 app.customer=customer 코드 꼭 넣기

--- src/main/java/com/vet/main/emp/EmpVO.java
+++ src/main/java/com/vet/main/emp/EmpVO.java
@@ -1,13 +1,6 @@
 package com.vet.main.emp;
 
 import java.sql.Date;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -16,10 +9,10 @@ import lombok.ToString;
 @Setter
 @Getter
 @ToString
-public class EmpVO implements UserDetails{
+public class EmpVO {
 
 	//emp
-	private String empNo;
+	private Long empNo;
 	private String password;
 	private String name;
 	private String email;
@@ -36,7 +29,6 @@ public class EmpVO implements UserDetails{
 	
 	//position
 	private String positionName;
-	private String role;
 	
 	//department
 	private String deptName;
@@ -44,52 +36,4 @@ public class EmpVO implements UserDetails{
 	private Long depth;
 	
 	private String passwordCheck;
-
-	private Boolean enabled;
-	private List<RoleVO> roleVOs;
-	
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		List<GrantedAuthority> authorities = new ArrayList<>();
-		
-		for(RoleVO roleVO:roleVOs) {
-			authorities.add(new SimpleGrantedAuthority(roleVO.getRole()));
-		}
-		
-		return authorities;
-	}
-
-	@Override
-	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	@Override
-	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		return true;
-	}
-
-	@Override
-	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
-		return true;
-	}
-
-	@Override
-	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
-		return true;
-	}
-
-	@Override
-	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return this.enabled;
-	}
-	
-	
-	
-	
 }
