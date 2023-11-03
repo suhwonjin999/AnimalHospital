2af8d976fd70b300609bc42d2d9600d920002949
Moment0129 <hrrr9400@gmail.com>
2023-11-03 09:12:10 +0900

    20231103

--- src/main/java/com/vet/main/attendance/AttendanceVO.java
+++ src/main/java/com/vet/main/attendance/AttendanceVO.java
@@ -1,5 +1,7 @@
 package com.vet.main.attendance;
 
+import java.time.LocalDateTime;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,5 +10,7 @@ import lombok.ToString;
 @Getter
 @ToString
 public class AttendanceVO {
-	private Long empNo;
+	private String username;
+	private LocalDateTime attIn;
+	private LocalDateTime attOut;
 }
